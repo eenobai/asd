3334aa9454b7b0d64bad85603771557f26674497
eenobai <67269852+eenobai@users.noreply.github.com>
2020-07-31 11:44:36 +0300

    sadsa

--- gs-serving-web-content/complete/src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
+++ gs-serving-web-content/complete/src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServingWebContentApplication {
 
+    public static void test(){
+        System.out.println("works");
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ServingWebContentApplication.class, args);
     }
---
+++ gs-serving-web-content/complete/src/main/resources/static/index.jsp
@@ -0,0 +1,3 @@
+<%@ page import="localhost:8080/index.html"%>
+
+<%= com.example.servingwebcontent.ServingWebContentApplication.test() %>
